5a24eb2fdb7616ea2183b464488e2bdccc6fdf39
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-10-27 12:06:49 +0530

    Added a basic inMemoryAuthentication with a simple login page.

---
+++ src/main/java/com/raj/springbootsecuritypractice/SpringBootSecurityPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.raj.springbootsecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityPracticeApplication.class, args);
+	}
+}
